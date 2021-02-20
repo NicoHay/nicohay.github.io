0271285631cd13546661671cc8b2922519178f80
hayni <hay.nico@hotmail.fr>
2021-02-20 18:22:47 +0100

    ajout scroll base

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
-console.log("hello")
+
+$( document ).ready(function() {
+
+   const  divToAnim = $('#intro-content')
+
+
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if(scroll > 250){
+            divToAnim.css('top', scroll * 1.8 );
+            if (scroll >= 600) {
+                divToAnim.fadeOut('slow')
+            }else{
+                divToAnim.fadeIn('slow')
+            }
+        }
+    });
+  
+
+
+
+
+
+});
