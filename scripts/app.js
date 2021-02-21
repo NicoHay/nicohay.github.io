604353d025d91248b97237e8a5873b7b9c605e22
hayni <hay.nico@hotmail.fr>
2021-02-21 17:32:22 +0100

    ajout scroll + menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,58 @@
 
-$( document ).ready(function() {
+const scroll_lottie = document.querySelector('#lottie-scroll');
+const lottie_burger = document.querySelector('#lottie-burger');
+const menu_box = document.querySelector('#menuBox');
 
-   const  divToAnim = $('#intro-content')
 
 
-    $(window).scroll(function (event) {
-        var scroll = $(window).scrollTop();
-        if(scroll > 250){
-            divToAnim.css('top', scroll * 1.8 );
-            if (scroll >= 600) {
-                divToAnim.fadeOut('slow')
-            }else{
-                divToAnim.fadeIn('slow')
-            }
+// lottie.loadAnimation({
+//     container: scroll_lottie, // the dom element that will contain the animation
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: './../images/scroll-animation.json' // the path to the animation json
+// });
+let menu_burger_animation = lottie.loadAnimation({
+    container: lottie_burger, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: './../images/menu-animation.json' // the path to the animation json
+});
+
+// menu animation
+lottie_burger.addEventListener('click', function (e) {
+
+    if (lottie_burger.classList.contains("closed-burger")) {
+        menu_box.classList.toggle('open-menu')
+        lottie_burger.classList.toggle("closed-burger")
+        menu_burger_animation.playSegments([0, 90], true)
+        lottie_burger.classList.add("opened-burger")
+
+    } else {
+        lottie_burger.classList.add("closed-burger")
+        menu_burger_animation.playSegments([90, 180], true)
+        lottie_burger.classList.toggle("opened-burger")
+        menu_box.classList.remove('open-menu')
+    }
+});
+
+
+window.addEventListener('scroll', function (e) {
+
+    const target = document.querySelectorAll('.scrollMe');
+
+    var index = 0, length = target.length;
+    for (index; index < length; index++) {
+        var pos = window.pageYOffset * target[index].dataset.speed;
+
+        if (target[index].dataset.direction === 'vertical') {
+            target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
+        } else {
+            var posX = window.pageYOffset * target[index].dataset.ratex;
+            var posY = window.pageYOffset * target[index].dataset.ratey;
+
+            target[index].style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0px)';
         }
-    });
-  
-
-
-
-
-
+    }
 });
