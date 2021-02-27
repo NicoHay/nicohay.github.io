c0ababb02a368e2b5e3e07fdeda9cc372c3c2f1e
hayni <hay.nico@hotmail.fr>
2021-02-27 19:29:51 +0100

    ok now

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,16 @@
-
-const scroll_lottie = document.querySelector('#lottie-scroll');
 const lottie_burger = document.querySelector('#lottie-burger');
+// const scroll_lottie = document.querySelector('#lottie-scroll');
 const menu_box = document.querySelector('#menuBox');
 
+var s = skrollr.init(
+    [
+        smoothScrolling = true,
+    ]
+);
 
-
-// lottie.loadAnimation({
-//     container: scroll_lottie, // the dom element that will contain the animation
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: './../images/scroll-animation.json' // the path to the animation json
-// });
+/**
+ * SVG DU BURGER 
+ */
 let menu_burger_animation = lottie.loadAnimation({
     container: lottie_burger, // the dom element that will contain the animation
     renderer: 'svg',
@@ -20,9 +19,9 @@ let menu_burger_animation = lottie.loadAnimation({
     path: './../images/menu-animation.json' // the path to the animation json
 });
 
-// menu animation
-lottie_burger.addEventListener('click', function (e) {
 
+// menu animation
+lottie_burger.addEventListener('click', function(e) {
     if (lottie_burger.classList.contains("closed-burger")) {
         menu_box.classList.toggle('open-menu')
         lottie_burger.classList.toggle("closed-burger")
@@ -34,25 +33,5 @@ lottie_burger.addEventListener('click', function (e) {
         menu_burger_animation.playSegments([90, 180], true)
         lottie_burger.classList.toggle("opened-burger")
         menu_box.classList.remove('open-menu')
-    }
-});
-
-
-window.addEventListener('scroll', function (e) {
-
-    const target = document.querySelectorAll('.scrollMe');
-
-    var index = 0, length = target.length;
-    for (index; index < length; index++) {
-        var pos = window.pageYOffset * target[index].dataset.speed;
-
-        if (target[index].dataset.direction === 'vertical') {
-            target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
-        } else {
-            var posX = window.pageYOffset * target[index].dataset.ratex;
-            var posY = window.pageYOffset * target[index].dataset.ratey;
-
-            target[index].style.transform = 'translate3d(' + posX + 'px, ' + posY + 'px, 0px)';
-        }
     }
 });
